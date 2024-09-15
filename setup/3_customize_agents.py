35a9b123c05b424cf277a2da7e484255ce89e516
Kenneth See <kennethseede@gmail.com>
2024-09-16 01:15:28 +0800

    initial setup

--- setup/3_customize_agents.py
+++ setup/3_customize_agents.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import inspect
+import textwrap
+from code_editor import code_editor
+from PSSimPy import Bank
 
 st.write('# Customize Agents')
+
+existing_strategy_implementation = inspect.getsource(Bank.strategy)
+existing_strategy_implementation = "\n".join(existing_strategy_implementation.splitlines()[1:])
+existing_strategy_implementation = textwrap.dedent(existing_strategy_implementation)
+
+custom_btns = [{
+  "name": "Submit",
+#   "feather": "Submit",
+  "hasText": True,
+  "alwaysOn": True,
+  "commands": ["submit"],
+  "style": {"top": "0.46rem", "right": "0.4rem"}
+}]
+new_strategy_implementation = code_editor(existing_strategy_implementation, height=[5, 1000], lang='python', buttons=custom_btns)
+print(new_strategy_implementation['text'])
