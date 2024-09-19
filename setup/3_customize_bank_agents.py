3ba470fc25c8edc8ff32d6a91d57eea7b36d9975
Kenneth See <kennethseede@gmail.com>
2024-09-20 00:32:27 +0800

    rename agents to Bank Agents

--- setup/3_customize_bank_agents.py
+++ setup/3_customize_bank_agents.py
@@ -4,7 +4,7 @@ import textwrap
 from code_editor import code_editor
 from PSSimPy import Bank
 
-st.write('# Customize Agents')
+st.write('# Customize Bank Agents')
 
 existing_strategy_implementation = inspect.getsource(Bank.strategy)
 existing_strategy_implementation = "\n".join(existing_strategy_implementation.splitlines()[1:])
@@ -19,4 +19,6 @@ custom_btns = [{
   "style": {"top": "0.46rem", "right": "0.4rem"}
 }]
 new_strategy_implementation = code_editor(existing_strategy_implementation, height=[5, 1000], lang='python', buttons=custom_btns)
-print(new_strategy_implementation['text'])
+if (existing_strategy_implementation != new_strategy_implementation['text']) and (existing_strategy_implementation != ''):
+    existing_strategy_implementation = new_strategy_implementation['text']
+    st.success('Strategy implementation updated')
