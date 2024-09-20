9e9342b4dd2461b5d54974df01eef6b4599fda8b
Kenneth See <kennethseede@gmail.com>
2024-09-21 01:24:00 +0800

    add additional agents

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ setup_input_page = st.Page("setup/2_input_data.py",
                            icon=":material/input:")
 setup_customize_agents_page = st.Page("setup/3_customize_bank_agents.py",
                            title="Customize Bank Agents",
-                           icon=":material/manage_accounts:")
-setup_preview_page = st.Page("setup/4_preview.py",
+                           icon=":material/account_balance:")
+setup_customize_settlement_agent_page = st.Page("setup/4_customize_settlement_agent.py",
+                             title="Customize Settlement Agent",
+                             icon=":material/sync_alt:")
+setup_customize_credit_facility_page = st.Page("setup/5_customize_credit_facility_agent.py",
+                             title="Customize Credit Facility Agent",
+                             icon=":material/currency_exchange:")
+setup_preview_page = st.Page("setup/6_preview.py",
                              title="Preview",
                              icon=":material/preview:")
 results_raw_page = st.Page("results/1_raw_data.py",
@@ -26,7 +32,7 @@ results_liquidity_page = st.Page("results/2_liquidity.py",
 pg = st.navigation(
     {
         "Landing": [landing_page],
-        "Setup": [setup_parameters_page, setup_input_page, setup_customize_agents_page, setup_preview_page],
+        "Setup": [setup_parameters_page, setup_input_page, setup_customize_agents_page, setup_customize_settlement_agent_page, setup_customize_credit_facility_page, setup_preview_page],
         "Reulsts": [results_raw_page, results_liquidity_page]
     }
 )
