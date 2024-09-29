e7603c464b650f15186bee93b43f3044b4bf8c29
Kenneth See <kennethseede@gmail.com>
2024-09-30 00:48:59 +0800

    update function header method to factor in static methods

--- utils/helper.py
+++ utils/helper.py
@@ -7,10 +7,15 @@ def initialize_dict_key(dictionary: dict, key, initialization_value):
     if key not in dictionary:
         dictionary[key] = initialization_value
 
-def get_function_header(func: Callable, is_abstract_method:bool=True) -> str:
+def get_function_header(func: Callable, is_abstract_method:bool=True, is_static_method:bool=False) -> str:
     """Gets the header of a given function"""
     # abstract methods will insclude "@abstractmethod" in the first row so that will need to be skipped
-    header_row_num = 1 if is_abstract_method else 0
+    # likewise with staticmethods
+    header_row_num = 0
+    if is_abstract_method:
+        header_row_num += 1
+    if is_static_method:
+        header_row_num += 1
     # extract function code as text
     function_text = inspect.getsource(func)
     # extract header row and format it
